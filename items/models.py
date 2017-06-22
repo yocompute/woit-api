a347206e6e7dff9e973adca3dc162243c471421d
zlkca <zlk1979@hotmail.com>
2017-06-21 21:39:03 -0400

    Fix list display issue and add more fields to item module.

--- items/models.py
+++ items/models.py
@@ -5,17 +5,20 @@ from django.db.models import CharField, Model, ForeignKey, DateTimeField, Decima
 
 TYPES = (('photo','Photo'), ('paint', 'Paint'))
 SOURCES = (('original','Original'), ('market', 'From Market'))
-
+CURRENCIES = (('usd','USD'), ('cad', 'CAD'), ('cny','CNY'))
+STYLES = (('contemporary', 'Contemporary'), ('modern', 'Modern'))
 class Item(Model):
     title = CharField(max_length=255, null=True, blank=True)
     description = CharField(max_length=1000, null=True, blank=True)
     code = CharField(max_length=255, null=True, blank=True)
     dimension = CharField(max_length=255, null=True, blank=True)
     author = CharField(max_length=255, null=True, blank=True)
-
-    type = CharField(max_length=4, choices=TYPES, default='photo')
-    source = CharField(max_length=4, choices=SOURCES, default='original')
+    date = CharField(max_length=64, null=True, blank=True)
+    type = CharField(max_length=32, choices=TYPES, default='photo')
+    source = CharField(max_length=32, choices=SOURCES, default='original')
+    style = CharField(max_length=128, choices=STYLES, default='contemporary')
     price = DecimalField(max_digits=10, decimal_places=2, null=True)
+    currency = CharField(max_length=16, choices=CURRENCIES, default='usd')
     n_copies = DecimalField(max_digits=10, decimal_places=2, null=True)
 
     fpath = CharField(max_length=1000, null=True, blank=True)
