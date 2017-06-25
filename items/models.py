66cba4b1842f843f89c37f4532bf3a343baa4543
zlkca <zlk1979@hotmail.com>
2017-06-25 08:21:50 -0400

    Add product table and items, orders, transactions table schema change

--- items/models.py
+++ items/models.py
@@ -7,26 +7,24 @@ TYPES = (('photo','Photo'), ('paint', 'Paint'))
 SOURCES = (('original','Original'), ('market', 'From Market'))
 CURRENCIES = (('usd','USD'), ('cad', 'CAD'), ('cny','CNY'))
 STYLES = (('contemporary', 'Contemporary'), ('modern', 'Modern'))
+STATUS = (('active','Active'), ('inactive', 'Inactive'))
 
 class Item(Model):
     title = CharField(max_length=255, null=True, blank=True)
     description = CharField(max_length=1000, null=True, blank=True)
-    code = CharField(max_length=255, null=True, blank=True)
     dimension = CharField(max_length=255, null=True, blank=True)
     author = CharField(max_length=255, null=True, blank=True)
     year = CharField(max_length=64, null=True, blank=True)
     type = CharField(max_length=32, choices=TYPES, default='photo')
     source = CharField(max_length=32, choices=SOURCES, default='original')
     style = CharField(max_length=128, choices=STYLES, default='contemporary')
-    price = DecimalField(max_digits=10, decimal_places=2, null=True)
-    currency = CharField(max_length=16, choices=CURRENCIES, default='usd')
     n_copies = DecimalField(max_digits=10, decimal_places=2, null=True)
 
     fpath = CharField(max_length=1000, null=True, blank=True)
     created = DateTimeField(auto_now_add=True)
     updated = DateTimeField(auto_now=True)
 
-    owner = ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    user = ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
 
     # def __str__(self):
     #     return self.title
@@ -34,38 +32,51 @@ class Item(Model):
     def __unicode__(self):
         return self.title
 
-    def owner_username(self):
-        return self.owner.username
+    def username(self):
+        return self.user.username
 
-
-OWNER_STATUS = (('active','Active'), ('inactive', 'Inactive'))
-
-class OwnerHistory(Model):
-    status = CharField(max_length=16, choices=OWNER_STATUS, default='active')
+# products is a table to represent selling list
+class Product(Model):
+    code = CharField(max_length=255, null=True, blank=True)
+    price = DecimalField(max_digits=10, decimal_places=3, null=True)
+    currency = CharField(max_length=16, choices=CURRENCIES, default='usd')
+    status = CharField(max_length=16, choices=STATUS, default='active')
     created = DateTimeField(auto_now_add=True)
     updated = DateTimeField(auto_now_add=True)
 
-    item = ForeignKey(Item, null=True, blank=True, on_delete=models.CASCADE)
     owner = ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    item = ForeignKey(Item, null=True, blank=True, on_delete=models.CASCADE)
+    
+class Ownership(Model):
+    source = CharField(max_length=32, choices=SOURCES, default='original')
+    buy_price = DecimalField(max_digits=10, decimal_places=3, null=True)
+    buy_currency = CharField(max_length=16, choices=CURRENCIES, default='usd')
+    sell_price = DecimalField(max_digits=10, decimal_places=3, null=True)
+    sell_currency = CharField(max_length=16, choices=CURRENCIES, default='usd')
+    status = CharField(max_length=16, choices=STATUS, default='active')
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now_add=True)
+
+    product = ForeignKey(Product, null=True, blank=True, on_delete=models.CASCADE)
+    owner = ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+
+
 
 class UploadForm(forms.Form):
     file = forms.ImageField()
     title = CharField(max_length=255, null=True, blank=True)
     description = CharField(max_length=1000, null=True, blank=True)
-    code = CharField(max_length=255, null=True, blank=True)
     dimension = CharField(max_length=255, null=True, blank=True)
     author = CharField(max_length=255, null=True, blank=True)
     year = CharField(max_length=64, null=True, blank=True)
     type = CharField(max_length=32, choices=TYPES, default='photo')
     source = CharField(max_length=32, choices=SOURCES, default='original')
     style = CharField(max_length=128, choices=STYLES, default='contemporary')
-    price = DecimalField(max_digits=10, decimal_places=2, null=True)
-    currency = CharField(max_length=16, choices=CURRENCIES, default='usd')
     n_copies = DecimalField(max_digits=10, decimal_places=2, null=True)
 
     fpath = CharField(max_length=1000, null=True, blank=True)
     created = DateTimeField(auto_now_add=True)
     updated = DateTimeField(auto_now=True)
 
-    owner = ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    user = ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
 
--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.db.models import CharField, Model, ForeignKey, DateTimeField, DecimalField
 from django.contrib.auth.models import User
-from items.models import Item
+from items.models import Product
 
 Status = (('completed','Completed'), ('pending', 'Pending'))
+CURRENCIES = (('usd','USD'), ('cad', 'CAD'), ('cny','CNY'))
 
 class Order(Model):
-    price = DecimalField(max_digits=10, decimal_places=3, null=True) 
+    price = DecimalField(max_digits=10, decimal_places=3, null=True)
+    currency = CharField(max_length=16, choices=CURRENCIES, default='usd') 
     discount = DecimalField(max_digits=10, decimal_places=3, null=True) 
     tax = DecimalField(max_digits=10, decimal_places=3, null=True) 
     total = DecimalField(max_digits=10, decimal_places=3, null=True) 
@@ -14,5 +16,5 @@ class Order(Model):
     created = DateTimeField(auto_now_add=True)
     updated = DateTimeField(auto_now=True)
 
-    item = ForeignKey(Item)
-    buyer = ForeignKey(User)
+    product = ForeignKey(Product, null=True, blank=True)
+    buyer = ForeignKey(User, null=True, blank=True)
